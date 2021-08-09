bb4397d4401e86a0aa536e89b3ec176c88f894b0
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-08-09 07:36:58 -0700

    class  Attribute

--- demo/basic/OOPS/fund.class.py
+++ demo/basic/OOPS/fund.class.py
@@ -1,34 +1,43 @@
 import __main__
 
-a,b=2,4;  c=a + b #  + is def __add__
+a,b=2,4;  c=a+b #  + is def __add__
 
 class Fund:
-    def __init__(self, name, number):
+    category = 'AM'
+    def __init__(self, name, number, share=10):
         self.name = name
         self.number = number
         self.active=False
-        self.share=10
+        self.share=share
         #return self
 
     def shareee(self):
-        return self.share
+        return self.share * 10
 
     def marketValue(self, price):
         return self.share * price
 
-tdf10 = Fund("TDF10", 11000061)
-print(tdf10.name, tdf10.number, type(tdf10))
+# update class attribute
+Fund.category = 'American Fund'
+
+tdf10 = Fund("TDF10", 11000061, share=20)
+tdf10.category ='AFIS'
+print(tdf10.name, tdf10.number,  tdf10.category,  tdf10.share,type(tdf10))
 print('share : ', tdf10.shareee())
 print('mkt: ', tdf10.marketValue(10))
 
-tdf20 = Fund('TDF20', 11000062); tdf20.name = 'XXXX'
-print(tdf20.name, tdf20.number, type(tdf10))
+tdf20 = Fund('TDF20', 11000062); tdf20.share = 50;
+print(tdf20.name, tdf20.number, tdf20.category, tdf20.share, type(tdf20))
 print()
 print('str format 1: {0}'.format(tdf20))
 print('str format 2: {0.name}:{0.number}'.format( tdf20))
 
+# print(tdf20.shareee(tdf10))
+
 s=str('hello Liu'); print(s, type(s))
 s='hello Liu'; print(s, type(s))
+
+
 
 # __main__ expriment
 print('\n===== __main__ ===========')
