ffefa9750ceffd0de51665fb2a4d63a1531673f9
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2021-08-10 06:39:46 -0700

    class  Attribute

--- demo/basic/OOPS/fund.class.py
+++ demo/basic/OOPS/fund.class.py
@@ -4,12 +4,23 @@ a,b=2,4;  c=a+b #  + is def __add__
 
 class Fund:
     category = 'AM'
+    self = None
     def __init__(self, name, number, share=10):
+        pass
+
+    def __init__(self, name, number, share=10):
+        #self = None
         self.name = name
         self.number = number
         self.active=False
         self.share=share
-        #return self
+        # self.new
+        print('&&&&&&&&&&&& COndtructor :: ' , self)
+        # return self
+
+    # order matte, second occurance will override above defination
+    # def __init__(self, name, number, share=10):
+    #     pass
 
     def shareee(self):
         return self.share * 10
@@ -21,10 +32,11 @@ class Fund:
 Fund.category = 'American Fund'
 
 tdf10 = Fund("TDF10", 11000061, share=20)
-tdf10.category ='AFIS'
-print(tdf10.name, tdf10.number,  tdf10.category,  tdf10.share,type(tdf10))
+tdf10.category ='AFIS' # add attribute same as class attribute
+Fund.category = 'American Fund again'
+print(tdf10.name, tdf10.number,  tdf10.category, Fund.category,  tdf10.share,type(tdf10) ) #, tdf10.new)
 print('share : ', tdf10.shareee())
-print('mkt: ', tdf10.marketValue(10))
+print('mkt : ', tdf10.marketValue(10))
 
 tdf20 = Fund('TDF20', 11000062); tdf20.share = 50;
 print(tdf20.name, tdf20.number, tdf20.category, tdf20.share, type(tdf20))
@@ -32,15 +44,23 @@ print()
 print('str format 1: {0}'.format(tdf20))
 print('str format 2: {0.name}:{0.number}'.format( tdf20))
 
+print(tdf10.__dict__)
+print(tdf20.__dict__)
 # print(tdf20.shareee(tdf10))
 
-s=str('hello Liu'); print(s, type(s))
-s='hello Liu'; print(s, type(s))
+s = str('hello Liu'); print(s, type(s))
+s = 'hello Liu'; print(s, type(s))
 
-
-
-# __main__ expriment
+print(Fund.self)
+# __main__ experiment
 print('\n===== __main__ ===========')
 print(__main__.tdf10)
 print(__main__.tdf10.__init__)
-__main__.tdf10.__init__('TDF30',11000063); print(tdf20.name, tdf20.number) # no impact
+__main__.tdf10.__init__('TDF30', 11000063);
+print(tdf20.name, tdf20.number) # no impact
+
+# if we try to access to static java feild with instnce  its gives error
+# but py does not error/warning while accessing class attribute with insatnce
+# rather it adds new feild  in itance with same name.
+# class attribute id differeent from static feild in java.
+# quite new thing for  java OOP developer
